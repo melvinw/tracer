2591c07baf2ca8a807876799729131e760c6eb39
Melvin Walls <mwalls67@gmail.com>
2022-03-26 23:53:10 -0700

    cast rays, allow arbitrary node nesting

--- src/main.cc
+++ src/main.cc
@@ -1,18 +1,28 @@
 #include <cassert>
+#include <cmath>
 #include <iostream>
 #include <map>
 #include <set>
 #include <string>
+#include <utility>
+#include <vector>
 
+#include <bvh/bounding_box.hpp>
 #include <bvh/bvh.hpp>
+#include <bvh/primitive_intersectors.hpp>
+#include <bvh/single_ray_traverser.hpp>
+#include <bvh/sphere.hpp>
 #include <bvh/sweep_sah_builder.hpp>
 #include <bvh/triangle.hpp>
 #include <bvh/vector.hpp>
 #include <tiny_gltf.h>
 
-using Vec3 = bvh::Vector3<float>;
-using Triangle = bvh::Triangle<float>;
-using Bvh = bvh::Bvh<float>;
+using Scalar = float;
+using Vec3 = bvh::Vector3<Scalar>;
+using Triangle = bvh::Triangle<Scalar>;
+using Bvh = bvh::Bvh<Scalar>;
+using BoundingBox = bvh::BoundingBox<Scalar>;
+using Sphere = bvh::Sphere<Scalar>;
 
 int main(int argc, char **argv) {
   assert(argc == 2);
@@ -39,27 +49,30 @@ int main(int argc, char **argv) {
   assert(model.scenes.size() == 1);
   const auto &scene = model.scenes[0];
   std::map<int, std::set<int>> mesh_nodes;
+  std::vector<int> front;
   for (const int nidx : scene.nodes) {
+    front.push_back(nidx);
+  }
+  while (!front.empty()) {
+    int nidx = front.back();
+    front.pop_back();
     assert(static_cast<size_t>(nidx) < model.nodes.size());
     const auto &node = model.nodes[nidx];
     if (node.mesh != -1) {
       mesh_nodes[node.mesh].insert(nidx);
     }
-    // TODO: allow more than one level of nesting
     for (const int cidx : node.children) {
-      assert(static_cast<size_t>(cidx) < model.nodes.size());
-      const auto &child = model.nodes[cidx];
-      if (child.mesh != -1) {
-        mesh_nodes[child.mesh].insert(cidx);
-      }
+      front.push_back(cidx);
     }
   }
 
   std::vector<bvh::Triangle<float>> triangles;
+  std::map<int, std::pair<size_t, size_t>> mesh_triangles;
   for (const auto &[midx, nodes] : mesh_nodes) {
     std::ignore = nodes;
     assert(static_cast<size_t>(midx) < model.meshes.size());
     const auto &mesh = model.meshes[midx];
+    size_t start = triangles.size();
     for (const auto &prim : mesh.primitives) {
       // TODO: support more than just triangles
       assert(prim.mode == TINYGLTF_MODE_TRIANGLES);
@@ -129,6 +142,8 @@ int main(int argc, char **argv) {
                                      vertices[indices[i + 2]]));
       }
     }
+    size_t end = triangles.size() - 1;
+    mesh_triangles[midx] = {start, end};
   }
 
   std::cerr << "found " << triangles.size() << " triangles. constructing BVH"
@@ -140,6 +155,60 @@ int main(int argc, char **argv) {
       bvh::compute_bounding_boxes_union(bboxes.get(), triangles.size());
   bvh::SweepSahBuilder<Bvh> builder(bvh);
   builder.build(global_bbox, bboxes.get(), centers.get(), triangles.size());
+
+  Scalar radius = bvh::length(global_bbox.diagonal());
+  Sphere bsphere(global_bbox.center(), radius);
+  std::cerr << "bounding sphere at (" << bsphere.origin[0] << ", "
+            << bsphere.origin[1] << ", " << bsphere.origin[2] << ") w/ radius "
+            << bsphere.radius << std::endl;
+
+  // TODO: Sun is fixed at high noon right now. Make this configurable.
+  Vec3 sun_center = bsphere.origin + Vec3(0, 0, bsphere.radius);
+  std::cerr << "sun disk at (" << sun_center[0] << ", " << sun_center[1] << ", "
+            << sun_center[2] << ")" << std::endl;
+
+  // First pass to accumulate light on each mesh
+  for (const auto &[midx, interval] : mesh_triangles) {
+    const auto &mesh = model.meshes[midx];
+    const auto [start, end] = interval;
+    std::cerr << "launching " << end - start << " rays from " << mesh.name
+              << std::endl;
+
+    size_t hits = 0;
+    size_t self_hits = 0;
+    size_t misses = 0;
+    for (size_t i = start; i <= end; i++) {
+      const auto &t = triangles[i];
+      const Vec3 center = t.center();
+
+      // TODO: cast to closest point on disk instead of center
+      bvh::Ray<Scalar> ray(center, center - sun_center, 0.0, bsphere.radius);
+      bvh::ClosestPrimitiveIntersector<Bvh, Triangle> primitive_intersector(
+          bvh, triangles.data());
+      bvh::SingleRayTraverser<Bvh> traverser(bvh);
+
+      if (auto hit = traverser.traverse(ray, primitive_intersector)) {
+        auto tidx = hit->primitive_index;
+        if (tidx >= start && tidx <= end) {
+          // Hit mesh we originated from
+          self_hits++;
+        } else {
+          // Hit another mesh
+          hits++;
+        }
+      } else {
+        // Miss! Saw the sun
+        misses++;
+      }
+    }
+    std::cerr << "\thits: " << hits << std::endl
+              << "\tself_hits: " << self_hits << std::endl
+              << "\tmisses: " << misses << std::endl;
+  }
+
+  // TODO: Second pass to scatter some portion of light form each surface
+
+  // TODO: output copy of gltf from user w/ radiance annotations
 
   return 0;
 }
