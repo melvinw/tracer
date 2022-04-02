3b58e44fdbe5618c232f81c1489bf4a1f38be1f8
Melvin Walls <mwalls67@gmail.com>
2022-04-02 15:08:39 -0700

    scripts/cloner: clone nodes instead of meshes
    This lets us copy scale/rotation vectors.

--- scripts/cloner.py
+++ scripts/cloner.py
@@ -12,13 +12,17 @@ def main(args):
     with open(args.gltf_path) as f:
         gltf = json.load(f)
 
-    src_mesh_idx, src_mesh = None, None
-    for i, mesh in enumerate(gltf["meshes"]):
-        if mesh["name"] == args.source_mesh:
-            src_mesh_idx = i
-            src_mesh = mesh
+    src_node_idx, src_node = None, None
+    for i, node in enumerate(gltf["nodes"]):
+        if node["name"] == args.source_node:
+            src_node_idx = i
+            src_node = node
             break
-    assert src_mesh is not None
+    assert src_node is not None
+    assert len(src_node.get("children", [])) == 0
+
+    src_mesh_idx = src_node["mesh"]
+    src_mesh = gltf["meshes"][src_mesh_idx]
     acc = gltf["accessors"][src_mesh["primitives"][0]["attributes"]["POSITION"]]
     mid_x = (acc["max"][0] - acc["min"][0]) / 2.0
     mid_y = (acc["max"][1] - acc["min"][1]) / 2.0
@@ -38,8 +42,16 @@ def main(args):
         xscale = x * cell_w + cell_mid_x
         yscale = y * cell_d + cell_mid_y
         trans = [xscale / mid_x, yscale / mid_y, 0.0]
-        name = f"{args.source_mesh}_clone{i}" if len(parts) < 3 else parts[2]
-        gltf["nodes"].append({"name": name, "mesh": src_mesh_idx, "translation": trans})
+        name = f"{args.source_node}_clone{i}" if len(parts) < 3 else parts[2]
+        gltf["nodes"].append(
+            {
+                "name": name,
+                "mesh": src_mesh_idx,
+                "translation": trans,
+                "rotation": src_node.get("rotation", None),
+                "scale": src_node.get("scale", None),
+            }
+        )
     print(json.dumps(gltf, indent=2))
 
 
@@ -48,7 +60,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "gltf_path", type=str, help="path to the GLTF scene to use as a base"
     )
-    parser.add_argument("source_mesh", type=str, help="name of the mesh to clone")
+    parser.add_argument("source_node", type=str, help="name of the node to clone")
     parser.add_argument(
         "--instance",
         action="append",
